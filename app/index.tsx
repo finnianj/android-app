b051a3e178aa3ee82bd5731bc1c90a0707588da7
finn <charltonjones.fj@gmailcom>
2024-09-03 15:46:24 +0200

    chore: Add CustomButton component for start button

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import beachImage from '@/assets/meditation-images/beach.webp';
 import { LinearGradient } from 'expo-linear-gradient';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
+import CustomButton from '@/components/CustomButton';
 
 const App = () => {
   return (
@@ -14,17 +15,26 @@ const App = () => {
         className='flex-1 fixed top-0 left-0 w-full h-full'
       >
       <LinearGradient className='flex-1' colors={['rgba(0,0,0,0.4)', 'rgba(0,0,0,0.8)']}>
-        <View>
-          <SafeAreaView className='flex px-1 justify-between'>
-            <Text className='text-white text-center text-4xl font-bold'>
-              Simple Meditation
-            </Text>
-            <Text className='text-white text-center text-lg mt-3'>
-              Getting wrecked 4 fun
-            </Text>
+          <SafeAreaView className='flex-1 mx-5 my-12 justify-between'>
+            <View>
+              <Text className='text-white text-center text-4xl font-bold'>
+                Simple Meditation
+              </Text>
+              <Text className='text-white text-center text-lg mt-3'>
+                stuff
+              </Text>
+            </View>
+
+            <View>
+              <CustomButton
+                onPress={() => console.log('Start')}
+                title='Start'
+              />
+            </View>
+
             <StatusBar style='light' />
           </SafeAreaView>
-        </View>
+          
       </LinearGradient>
       </ImageBackground>
     </View>
---
+++ components/CustomButton.tsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { Text, TouchableOpacity, View } from 'react-native'
+
+interface CustomButtonProps {
+    onPress: () => void;
+    title: string;
+    textStyles?: string;
+    containerStyles?: string;
+}
+
+export default function CustomButton({ onPress, title, textStyles = '', containerStyles = '' }: CustomButtonProps) {
+    return (
+        <TouchableOpacity 
+            activeOpacity={0.7}
+            onPress={onPress}
+            className={`bg-white rounded-xl min-h-[62px] justify-center items-center ${containerStyles}`}
+        >
+            <Text className={`text-lg font-semibold ${textStyles}`}>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+
