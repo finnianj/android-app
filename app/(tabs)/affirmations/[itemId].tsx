b1b00c6f370324a742b630dfc76db54171c8a6c8
finn <charltonjones.fj@gmailcom>
2024-09-04 15:23:37 +0200

    feat: Add lazy loading for affirmation images in AffirmationPractice component

--- app/(tabs)/affirmations/[itemId].tsx
+++ app/(tabs)/affirmations/[itemId].tsx
@@ -1,13 +1,17 @@
 import { GalleryPreviewData } from '@/constants/models/AffirmationCategory';
 import { useLocalSearchParams } from 'expo-router';
 import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ImageBackground, ImageSourcePropType, Pressable, ScrollView } from 'react-native';
 import AFFIRMATION_GALLERY from '@/constants/affirmation-gallery';
+import AppGradient from '@/components/AppGradient';
+import { router } from 'expo-router';
+import { AntDesign } from '@expo/vector-icons';
 
 const AffirmationPractice = () => {
     const { itemId } =  useLocalSearchParams();
 
     const [affirmation, setAffirmation] = useState<GalleryPreviewData>();
+    const [sentences, setSentences] = useState<string[]>([]);
     
     useEffect(() => {
         for (let idx = 0; idx < AFFIRMATION_GALLERY.length; idx++) {
@@ -15,14 +19,42 @@ const AffirmationPractice = () => {
             const affirmationToStart = affirmationData.find((affirmation) => affirmation.id === Number(itemId));
             if (affirmationToStart) {
                 setAffirmation(affirmationToStart);
+                const affirmationArray = affirmationToStart.text.split('.');
+
+                // Remoce the last empty string
+                if (affirmationArray[affirmationArray.length - 1] === '') {
+                    affirmationArray.pop();
+                }
+
+                setSentences(affirmationArray);
                 return;
             }
         }
     }, [itemId]);
 
     return (
-        <View>
-        <Text>Affirmation Practice</Text>
+        <View className='flex-1'>
+            <ImageBackground 
+                source={affirmation?.image as ImageSourcePropType} 
+                resizeMode='cover' 
+                className='flex-1'
+            >
+                <AppGradient colors={['rgba(0,0,0,0.1)', 'rgba(0,0,0,0.9)']}>
+                    <Pressable onPress={() => router.back()} className='absolute top-16 left-6 z-10'>
+                        <AntDesign name='leftcircleo' size={50} color='white' />
+                    </Pressable>
+
+                    <ScrollView className='mt-20' showsVerticalScrollIndicator={false}>
+                        <View className='h-full justify-center'>
+                            <View className='h-4/5 justify-center'>
+                                {sentences.map((sentence, idx) => (
+                                    <Text key={idx} className='text-white text-2xl font-bold text-center mb-4'>{sentence}.</Text>
+                                ))}
+                            </View>
+                        </View>
+                    </ScrollView>
+                </AppGradient>
+            </ImageBackground>
         </View>
     );
 };
